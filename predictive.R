e2dcb95d5bdaffd46d7e47dcc5d26d9e8b69b247
進撃の雯雯老師 <askahu6@gmail.com>
2017-04-06 00:10:12 -0500

    Update predictive.R

--- predictive.R
+++ predictive.R
@@ -9,18 +9,27 @@ sum(diag(vali.lda))/sum(vali.lda)
 
 library(xgboost)
 model.xgb <- xgboost(
-	data=as.matrix(mf.wh[,-104]),
-	label=mf.wh[,104],
-	eta = 0.1,
-	max_depth = 15, 
-	nround=25, 
-	subsample = 0.5,
-	colsample_bytree = 0.5,
-	seed = 1,
-	eval_metric = "merror",
-	objective = "multi:softprob",
-	num_class = 12,
-	nthread = 3)
-vali.xgb <- table(predict(model.xgb,as.matrix(mf.wh[,-104])),mf.wh$VTE_t1)
+	data = data.matrix(mf.wh[,-104]), 
+	label = mf.wh[,104], 
+	max.depth = 5, 
+	eta = 1,
+	nround = 2, 
+	objective = "binary:logistic")
+pred.xgb <- predict(model.xgb,data.matrix(mf.wh[,-104]))
+model.xgb2 <- MASS::lda(mf.wh$VTE_t1~pred.xgb)
+pred.xgb2 <- predict(model.xgb2)
+vali.xgb <- table(pred.xgb2$class,mf.wh$VTE_t1)
 sum(diag(vali.xgb))/sum(vali.xgb)
 
+library(mxnet)
+model.mlp <- mx.mlp(
+	data.matrix(mf.wh[,-104]), 
+	mf.wh[,104], 
+	hidden_node=10, 
+	out_node=2,      
+	out_activation="softmax", 
+	num.round=20, 
+	array.batch.size=15, 
+	learning.rate=0.07, 
+	momentum=0.9, 
+	eval.metric=mx.metric.accuracy)
